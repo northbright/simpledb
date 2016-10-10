890d46adbde808934fcb5cf3feb4c616b438f598
Frank Xu <frank.xu@northbright.com>
2016-10-10 13:24:31 +0800

    Update package description.

--- doc.go
+++ doc.go
@@ -1,4 +1,4 @@
 /*
-simpledb is a simple database which provides CRUD and search operations on records stored in Redis.
+simpledb is a Golang package which provides CRUD and search operations on records stored in Redis. It's based on Redigo.
 */
 package simpledb
